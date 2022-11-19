e3f887ffd1fa03b0a282b801e815a0437ac5075e
xu756 <756334744@qq.com>
2022-11-19 23:39:29 +0800

    组件配置

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,9 @@
 <template>
-  aa
+  <a-button type="primary">Primary Button</a-button>
+  <a-button>Default Button</a-button>
+  <a-button type="dashed">Dashed Button</a-button>
+  <a-button type="text">Text Button</a-button>
+  <a-button type="link">Link Button</a-button>
 </template>
 
 <script setup lang="ts">
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import Components from 'unplugin-vue-components/vite'
+import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [
+        vue(),
+        Components({
+            dirs: ['src/pages','src/components'], // 配置需要默认导入的自定义组件文件夹，该文件夹下的所有组件都会自动 import
+            resolvers: [AntDesignVueResolver({importStyle: true, resolveIcons: true})],
+            // 加上下面这一行作为配置项即可
+            directoryAsNamespace: true,
+        })
+    ]
 })
